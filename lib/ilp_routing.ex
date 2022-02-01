3eb3de1f577e9bf8627c2edc0581800d5c520691
Riccardo Zanei <zanei.riccardo2812@gmail.com>
2022-02-01 14:48:05 +0100

    Initial commit

---
+++ lib/ilp_routing.ex
@@ -0,0 +1,18 @@
+defmodule IlpRouting do
+  @moduledoc """
+  Documentation for `IlpRouting`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IlpRouting.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ilp_routing_test.exs
@@ -0,0 +1,8 @@
+defmodule IlpRoutingTest do
+  use ExUnit.Case
+  doctest IlpRouting
+
+  test "greets the world" do
+    assert IlpRouting.hello() == :world
+  end
+end
