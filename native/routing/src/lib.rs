bb77d83c5b3f257b224a0bf792f86d73efcfcf13
Dominic Perini <dominic.perini@gmail.com>
2022-02-03 15:01:48 +0000

    fix

--- native/routing/src/lib.rs
+++ native/routing/src/lib.rs
@@ -1,8 +1,10 @@
-
+use rustler::types::atom::{ok};
+use rustler::types::binary::{Binary};
+use rustler::{Encoder, Env, Term, NifResult};
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn decode<'a>(env: Env<'a>,_bin: Binary) -> NifResult<Term<'a>> {
     Ok(ok().encode(env))
 }
-rustler::init!("Elixir.Routing", [decode]);
+rustler::init!("Elixir.IlpRouting", [decode]);
 
