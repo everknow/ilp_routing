1b1a4e350584a11ecadfb307c140e82a71a3f3bd
Riccardo Zanei <rzanei@forte.io>
2022-02-03 15:51:42 +0100

    test

--- lib/ilp_routing.ex
+++ lib/ilp_routing.ex
@@ -1,5 +1,7 @@
 defmodule IlpRouting do
 
+  use Rustler, otp_app: :ilp_routing, crate: "routing"
+
   def decode(_), do: :erlang.nif_error(:nif_not_loaded)
   def encode(_), do: :erlang.nif_error(:nif_not_loaded)
 
--- native/routing/src/lib.rs
+++ native/routing/src/lib.rs
@@ -1,3 +1,5 @@
+
+
 #[rustler::nif(schedule = "DirtyCpu")]
 fn decode<'a>(env: Env<'a>,_bin: Binary) -> NifResult<Term<'a>> {
     Ok(ok().encode(env))
