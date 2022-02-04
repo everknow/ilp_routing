364834f00e6140a6f30b48db8b9eb91790627a6e
Dominic Perini <dominic.perini@gmail.com>
2022-02-04 15:34:02 +0000

    WIP

--- native/routing/src/lib.rs
+++ native/routing/src/lib.rs
@@ -1,32 +1,37 @@
-use rustler::types::atom::{error, ok};
+use rustler::types::atom::{error}; //, ok};
 use rustler::types::binary::{Binary};
 use rustler::{Encoder, Env, Term, NifResult, MapIterator};
-use interledger_packet::{Packet, Address, PrepareBuilder};
+use interledger_packet::{Packet};
+use interledger_ccp::{RouteControlRequest, Mode};
 use bytes::{BytesMut};
 use std::convert::TryFrom;
-use once_cell::sync::Lazy;
-use std::str::FromStr;
+// use once_cell::sync::Lazy;
+// use std::str::FromStr;
 use std::collections::HashMap;
-use std::time::{Duration, SystemTime};
+// use std::time::{Duration, SystemTime};
 
-pub static CCP_CONTROL_DESTINATION: Lazy<Address> =
-    Lazy::new(|| Address::from_str("peer.route.control").unwrap());
-pub static CCP_UPDATE_DESTINATION: Lazy<Address> =
-    Lazy::new(|| Address::from_str("peer.route.update").unwrap());
+// pub static CCP_CONTROL_DESTINATION: Lazy<Address> =
+//     Lazy::new(|| Address::from_str("peer.route.control").unwrap());
+// pub static CCP_UPDATE_DESTINATION: Lazy<Address> =
+//     Lazy::new(|| Address::from_str("peer.route.update").unwrap());
+// pub const PEER_PROTOCOL_CONDITION: [u8; 32] = [
+//     102, 104, 122, 173, 248, 98, 189, 119, 108, 143, 193, 139, 142, 159, 142, 32, 8, 151, 20, 133,
+//     110, 226, 51, 179, 144, 42, 89, 29, 13, 95, 41, 37,
+// ];
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn decode<'a>(env: Env<'a>, bin: Binary) -> NifResult<Term<'a>> {
     match Packet::try_from(BytesMut::from(bin.as_slice())) {
-        Ok(Packet::Prepare(p)) => {
+        Ok(Packet::Prepare(_p)) => {
             
-            let destination = p.destination();
-            if destination == *CCP_CONTROL_DESTINATION {
-                Ok(custom_atoms::control().encode(env)) 
-            } else if destination == *CCP_UPDATE_DESTINATION {
-                Ok(custom_atoms::update().encode(env))
-            } else {
+            // let destination = p.destination();
+            // if destination == *CCP_CONTROL_DESTINATION {
+            //     Ok(custom_atoms::control().encode(env)) 
+            // } else if destination == *CCP_UPDATE_DESTINATION {
+            //     Ok(custom_atoms::update().encode(env))
+            // } else {
                 Ok(error().encode(env)) 
-            }
+            // }
 
 
             // match p.destination() {
@@ -70,6 +75,18 @@ fn encode<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
                     if &t.into_binary().unwrap().as_slice() == &b"prepare".as_slice() {
                         match m.get(b"data".as_ref()) {
                             Some(d) if d.is_binary() => {
+                                
+                                let data = RouteControlRequest {
+                                    features: Vec::new(),
+                                    last_known_epoch: 10,
+                                    last_known_routing_table_id: [0,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6],
+                                    mode: Mode::Sync
+
+
+                                };
+
+                                let p = data.to_prepare();
+                                
                                 // let p = PrepareBuilder {
                                 //     destination: CCP_CONTROL_DESTINATION.clone(),
                                 //     amount: 0,
@@ -79,7 +96,7 @@ fn encode<'a>(env: Env<'a>, arg: Term) -> NifResult<Term<'a>> {
                                 // }
                                 // .build();
 
-                                Ok(ok().encode(env))
+                                Ok(p.as_ref().encode(env))
                             }
                             Some(_) => {
                                 Ok((error(), "data val is not binary").encode(env)) 
