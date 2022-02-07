831911e611b29bc0a9d471053c84f7bca487faf5
Dominic Perini <dominic.perini@gmail.com>
2022-02-07 10:19:36 +0000

    wip test

--- test/ilp_routing_test.exs
+++ test/ilp_routing_test.exs
@@ -2,7 +2,36 @@ defmodule IlpRoutingTest do
   use ExUnit.Case
   doctest IlpRouting
 
-  test "greets the world" do
-    assert IlpRouting.hello() == :world
+  test "encode/1" do
+
+    assert {:error, "type missing"} = IlpRouting.encode(%{})
+
+    assert {:error, "arg ******"} = IlpRouting.encode(%{type: :some})
+
+    assert {:error, "type not binary"} = IlpRouting.encode(%{"type" => :some})
+
+    assert {:error, "type not recognised"} = IlpRouting.encode(%{"type" => "hello"})
+
+    assert {:error, "control_request > features missing"} = IlpRouting.encode(%{"type" => "control_request"})
+
+    assert {:error, "arg ******"} = IlpRouting.encode(%{"type" => "control_request", :features => :some})
+
+    assert {:error, "could not decode last_known_routing_table_id"} = IlpRouting.encode(%{
+      "type" => "control_request",
+      "features" => :some,
+      "last_known_epoch" => :some,
+      "last_known_routing_table_id" => :some,
+      "mode" => :some
+      })
+
+    assert {:error, ""} = IlpRouting.encode(%{
+      "type" => "control_request",
+      "features" => ["hello", "there"],
+      "last_known_epoch" => 345345,
+      "last_known_routing_table_id" => [0],
+      "mode" => 0
+      })
+
+
   end
 end
